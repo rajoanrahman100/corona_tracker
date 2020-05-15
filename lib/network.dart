96f7c2be7d065ef9b4af317d71d537e0411079ef
rokkhi-rajoan <rajoan@rokkhi.com>
2020-05-15 15:57:40 +0600

    emergency contact, total test done!!

--- lib/network.dart
+++ lib/network.dart
@@ -22,3 +22,188 @@ class NetworkHelper{
   }
 
 }
+
+
+ /*Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .35,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/corona.jpg"),
+                    fit: BoxFit.cover)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "Covid-19 Tracker",
+                      style: GoogleFonts.ubuntu(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      FlatButton(
+                        onPressed:()async{
+                          var typesCountry=await Navigator.push(context, MaterialPageRoute(builder: (context) {
+                            return CountryScreen();
+                          }));
+
+                          getCountryData(typesCountry);
+                        },
+                        child: Text(
+                          '$countrydata',
+                          style: GoogleFonts.ubuntu(
+                              fontSize: 24.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                      Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "Last Update 1 hour ago",
+                      style: GoogleFonts.ubuntu(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 5.0,
+                              spreadRadius: 0.1,
+                              offset: Offset(2, 2))
+                        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Grid(
+                          title: "Confirmed",
+                          no: '$cases',
+                          color: 0xff2AA847,
+                          icon: Icons.arrow_upward,
+                          today: '$todaycases',
+                        ),
+                        Grid(
+                          title: "Recovered",
+                          no: '$recovered',
+                          color: 0xff077eff,
+                          icon: Icons.arrow_upward,
+                          today: "",
+                        ),
+                        Grid(
+                          title: "Death",
+                          no: '$death',
+                          color: 0xffFe083b,
+                          icon: Icons.arrow_downward,
+                          today: '$todaydeath',
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    "Global Outbreak",
+                    style: GoogleFonts.ubuntu(
+                        fontSize: 22.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 5.0,
+                              spreadRadius: 0.1,
+                              offset: Offset(2, 2))
+                        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Grid(
+                          title: "Confirmed",
+                          no: '$globalcases',
+                          color: 0xff2AA847,
+                          icon: Icons.arrow_upward,
+                          today: '$globaltodaycases',
+                        ),
+                        Grid(
+                          title: "Recovered",
+                          no: '$globalrecovered',
+                          color: 0xff077eff,
+                          icon: Icons.arrow_upward,
+                          today: "",
+                        ),
+                        Grid(
+                          title: "Death",
+                          no: '$globaldeath',
+                          color: 0xffFe083b,
+                          icon: Icons.arrow_downward,
+                          today: '$globaltodaydeath',
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "Prevention",
+                      style: GoogleFonts.ubuntu(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 3,
+                      childAspectRatio: itemWidth / itemHeight,
+                      children: <Widget>[
+                        Prevention(title: "Cover your cough",image: "images/cough.png",),
+                        Prevention(title: "Wear a mask",image: "images/mask.png",),
+                        Prevention(title: "Keep safe distance",image: "images/distance.png",),
+                        Prevention(title: "Wash your hands",image: "images/hands.png",),
+                        Prevention(title: "Use hand wash",image: "images/spray.png",),
+                        Prevention(title: "Stay home",image: "images/home.png",)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),*/
--- lib/ui/after_splash.dart
+++ lib/ui/after_splash.dart
@@ -1,84 +1,82 @@
 import 'package:corona_tracker/ui/countryscreen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../network.dart';
-import 'home_screen.dart';
+import 'coronahelp.dart';
 
 class AfterSplash extends StatefulWidget {
-
   final country;
   final global;
 
-  AfterSplash({this.country,this.global});
+  AfterSplash({this.country, this.global});
 
   @override
   _AfterSplashState createState() => _AfterSplashState();
 }
 
 class _AfterSplashState extends State<AfterSplash> {
-
   String countrydata;
   int cases;
   int recovered;
   int death;
   int todaycases;
   int todaydeath;
+  int totaltests;
+  int testPerMill;
 
   int globalcases;
   int globalrecovered;
   int globaldeath;
   int globaltodaycases;
   int globaltodaydeath;
+  int globalTest;
+  double globalTestPerMill;
 
-  void updateUi(dynamic countryData){
-    countrydata=countryData['country'];
-    cases=countryData['cases'];
-    recovered=countryData['recovered'];
-    death=countryData['deaths'];
-    todaycases=countryData['todayCases'];
-    todaydeath=countryData['todayDeaths'];
-
+  void updateUi(dynamic countryData) {
+    countrydata = countryData['country'];
+    cases = countryData['cases'];
+    recovered = countryData['recovered'];
+    death = countryData['deaths'];
+    todaycases = countryData['todayCases'];
+    todaydeath = countryData['todayDeaths'];
+    totaltests = countryData['tests'];
+    testPerMill=countryData['testsPerOneMillion'];
   }
 
-
-  void updateUiCountry(dynamic countryData){
-
+  void updateUiCountry(dynamic countryData) {
     setState(() {
-      countrydata=countryData['country'];
-      cases=countryData['cases'];
-      recovered=countryData['recovered'];
-      death=countryData['deaths'];
-      todaycases=countryData['todayCases'];
-      todaydeath=countryData['todayDeaths'];
+      countrydata = countryData['country'];
+      cases = countryData['cases'];
+      recovered = countryData['recovered'];
+      death = countryData['deaths'];
+      todaycases = countryData['todayCases'];
+      todaydeath = countryData['todayDeaths'];
+      totaltests = countryData['tests'];
+      testPerMill=countryData['testsPerOneMillion'];
     });
-
-
   }
 
-  void updateUiGlobal(dynamic countryData){
-
-    globalcases=countryData['cases'];
-    globalrecovered=countryData['recovered'];
-    globaldeath=countryData['deaths'];
-    globaltodaycases=countryData['todayCases'];
-    globaltodaydeath=countryData['todayDeaths'];
-
+  void updateUiGlobal(dynamic countryData) {
+    globalcases = countryData['cases'];
+    globalrecovered = countryData['recovered'];
+    globaldeath = countryData['deaths'];
+    globaltodaycases = countryData['todayCases'];
+    globaltodaydeath = countryData['todayDeaths'];
+    globalTest=countryData['tests'];
+    globalTestPerMill=countryData['testsPerOneMillion'];
   }
-
-
 
   void getCountryData(String countryName) async {
     NetworkHelper networkHelper = NetworkHelper(
         url:
-        'https://disease.sh/v2/countries/$countryName?yesterday=false&strict=true');
+            'https://disease.sh/v2/countries/$countryName?yesterday=false&strict=true');
 
     var countryData = await networkHelper.getData();
 
     updateUiCountry(countryData);
-
   }
-
 
   @override
   void initState() {
@@ -89,7 +87,6 @@ class _AfterSplashState extends State<AfterSplash> {
     updateUiCountry(widget.country);
   }
 
-
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -98,38 +95,55 @@ class _AfterSplashState extends State<AfterSplash> {
     final double itemWidth = size.width / 2;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: size.height * .35,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/page_bg.jpg"),
-                    fit: BoxFit.cover)),
-          ),
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text("Covid-19 Tracker",style: GoogleFonts.ubuntu(
+          fontWeight: FontWeight.w500,
+          color: Colors.redAccent
+        ),),
+        actions: <Widget>[
           Padding(
-            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-            child: SafeArea(
+            padding: const EdgeInsets.only(right:8.0),
+            child: GestureDetector(onTap:(){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) {
+                    return CoronaHelpScreen();
+                  }));
+            },child: Icon(Icons.help,color: Colors.redAccent,)),
+          )
+        ],
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: size.height * .35,
+              width: double.infinity,
+              /*decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("images/corona.jpg"),
+                          fit: BoxFit.cover))*/
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Padding(
                     padding: const EdgeInsets.only(top: 10.0),
                     child: Text(
-                      "Covid-19 Tracker",
+                      "Select a country",
                       style: GoogleFonts.ubuntu(
                           fontSize: 17.0,
                           fontWeight: FontWeight.w400,
-                          color: Colors.white),
+                          color: Colors.black),
                     ),
                   ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       FlatButton(
-                        onPressed:()async{
-                          var typesCountry=await Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        onPressed: () async {
+                          var typesCountry = await Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
                             return CountryScreen();
                           }));
 
@@ -138,14 +152,14 @@ class _AfterSplashState extends State<AfterSplash> {
                         child: Text(
                           '$countrydata',
                           style: GoogleFonts.ubuntu(
-                              fontSize: 22.0,
-                              color: Colors.white,
+                              fontSize: 24.0,
+                              color: Colors.black,
                               fontWeight: FontWeight.w600),
                         ),
                       ),
                       Icon(
                         Icons.keyboard_arrow_down,
-                        color: Colors.white,
+                        color: Colors.black,
                       )
                     ],
                   ),
@@ -160,9 +174,10 @@ class _AfterSplashState extends State<AfterSplash> {
                     ),
                   ),
                   SizedBox(
-                    height: 20.0,
+                    height: 10.0,
                   ),
                   Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10.0),
                     decoration: BoxDecoration(
                         boxShadow: [
                           BoxShadow(
@@ -176,121 +191,140 @@ class _AfterSplashState extends State<AfterSplash> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
-                        Grid(
+                        GridBD(
                           title: "Confirmed",
                           no: '$cases',
                           color: 0xff2AA847,
                           icon: Icons.arrow_upward,
                           today: '$todaycases',
                         ),
-                        Grid(
+                        GridBD(
                           title: "Recovered",
                           no: '$recovered',
                           color: 0xff077eff,
                           icon: Icons.arrow_upward,
                           today: "",
                         ),
-                        Grid(
+                        GridBD(
                           title: "Death",
                           no: '$death',
                           color: 0xffFe083b,
                           icon: Icons.arrow_downward,
                           today: '$todaydeath',
                         ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Text(
-                    "Global Outbreak",
-                    style: GoogleFonts.ubuntu(
-                        fontSize: 22.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 5.0,
-                              spreadRadius: 0.1,
-                              offset: Offset(2, 2))
-                        ],
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10.0)),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Grid(
-                          title: "Confirmed",
-                          no: '$globalcases',
-                          color: 0xff2AA847,
-                          icon: Icons.arrow_upward,
-                          today: '$globaltodaycases',
-                        ),
-                        Grid(
-                          title: "Recovered",
-                          no: '$globalrecovered',
+                        GridBD(
+                          title: "TotalTests",
+                          no: '$totaltests',
                           color: 0xff077eff,
-                          icon: Icons.arrow_upward,
-                          today: "",
+                          icon: Icons.arrow_forward,
+                          today: '$testPerMill',
                         ),
-                        Grid(
-                          title: "Death",
-                          no: '$globaldeath',
-                          color: 0xffFe083b,
-                          icon: Icons.arrow_downward,
-                          today: '$globaltodaydeath',
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: Text(
-                      "Prevention",
-                      style: GoogleFonts.ubuntu(
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black),
-                    ),
-                  ),
-                  Expanded(
-                    child: GridView.count(
-                      crossAxisCount: 3,
-                      childAspectRatio: itemWidth / itemHeight,
-                      children: <Widget>[
-                        Prevention(title: "Cover your cough",image: "images/cough.png",),
-                        Prevention(title: "Wear a mask",image: "images/mask.png",),
-                        Prevention(title: "Keep safe distance",image: "images/distance.png",),
-                        Prevention(title: "Wash your hands",image: "images/hands.png",),
-                        Prevention(title: "Use hand wash",image: "images/spray.png",),
-                        Prevention(title: "Stay home",image: "images/home.png",)
+
                       ],
                     ),
                   )
                 ],
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                "Global Outbreak",
+                style: GoogleFonts.ubuntu(
+                    fontSize: 22.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            Container(
+             height: 110,
+              margin: EdgeInsets.symmetric(horizontal: 10.0),
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 5.0,
+                        spreadRadius: 0.1,
+                        offset: Offset(2, 2))
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Grid(
+                    title: "Confirmed",
+                    no: '$globalcases',
+                    color: 0xff2AA847,
+                    icon: Icons.arrow_upward,
+                    today: '$globaltodaycases',
+                  ),
+                  Grid(
+                    title: "Recovered",
+                    no: '$globalrecovered',
+                    color: 0xff077eff,
+                    icon: Icons.arrow_upward,
+                    today: "",
+                  ),
+                  Grid(
+                    title: "Death",
+                    no: '$globaldeath',
+                    color: 0xffFe083b,
+                    icon: Icons.arrow_downward,
+                    today: '$globaltodaydeath',
+                  ),
+                  Grid(
+                    title: "TotalTests",
+                    no: '$globalTest',
+                    color: 0xff077eff,
+                    icon: Icons.arrow_forward,
+                    today: '$globalTestPerMill',
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0,top: 10.0),
+              child: Text(
+                "Prevention",
+                style: GoogleFonts.ubuntu(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black),
+              ),
+            ),
+
+            Container(
+              height: 250.0,
+              child: GridView.count(
+                crossAxisCount: 3,
+                childAspectRatio: itemWidth / itemHeight,
+                children: <Widget>[
+                  Prevention(title: "Cover your cough",image: "images/cough.png",),
+                  Prevention(title: "Wear a mask",image: "images/mask.png",),
+                  Prevention(title: "Keep safe distance",image: "images/distance.png",),
+                  Prevention(title: "Wash your hands",image: "images/hands.png",),
+                  Prevention(title: "Use hand wash",image: "images/spray.png",),
+                  Prevention(title: "Stay home",image: "images/home.png",)
+                ],
+              ),
+            )
+
+          ],
+        ),
       ),
     );
   }
 }
 
 class Prevention extends StatelessWidget {
-
   final String title;
   final String image;
 
-  Prevention({this.title,this.image});
+  Prevention({this.title, this.image});
 
   @override
   Widget build(BuildContext context) {
@@ -315,26 +349,95 @@ class Grid extends StatelessWidget {
   final int color;
   final IconData icon;
   final String today;
+  final String totalTests;
 
-  Grid({this.title, this.no, this.color, this.icon, this.today});
+
+  Grid({this.title, this.no, this.color, this.icon, this.today,this.totalTests});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 5.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Text(
+            title,
+            style:
+                GoogleFonts.ubuntu(fontSize: 15.0, fontWeight: FontWeight.w500),
+          ),
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Text(
+            no,
+            style: GoogleFonts.ubuntu(fontSize: 15.0, color: Color(color)),
+          ),
+        ),
+        SizedBox(
+          height: 5.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Row(
+            children: <Widget>[
+              Icon(
+                icon,
+                size: 15.0,
+                color: Color(color),
+              ),
+              Text(
+                today,
+                style: GoogleFonts.ubuntu(
+                    fontWeight: FontWeight.bold, color: Color(color)),
+              )
+            ],
+          ),
+        ),
+
+
+
+        SizedBox(
+          height: 10.0,
+        ),
+      ],
+    );
+  }
+}
+
+class GridBD extends StatelessWidget {
+  final String title;
+  final String no;
+  final int color;
+  final IconData icon;
+  final String today;
+  final String totaltests;
+
+
+  GridBD({this.title, this.no, this.color, this.icon, this.today,this.totaltests});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(8.0),
           child: Text(
             title,
             style:
-                GoogleFonts.ubuntu(fontSize: 18.0, fontWeight: FontWeight.w500),
+            GoogleFonts.ubuntu(fontSize: 15.0, fontWeight: FontWeight.w500),
           ),
         ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
             no,
-            style: GoogleFonts.ubuntu(fontSize: 25.0, color: Color(color)),
+            style: GoogleFonts.ubuntu(fontSize: 18.0, color: Color(color)),
           ),
         ),
         Padding(
@@ -354,8 +457,12 @@ class Grid extends StatelessWidget {
             ],
           ),
         ),
+
+
+
+
         SizedBox(
-          height: 20.0,
+          height: 10.0,
         ),
       ],
     );
